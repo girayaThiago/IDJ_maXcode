832e717371c49202f059932bec51ccc5319902cb
Thiago Dantas <giraya.thiago@gmail.com>
2018-03-15 00:20:32 -0300

    adicionando main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  IDJ_2018
+//
+//  Created by Thiago Dantas on 11/03/18.
+//  Copyright © 2018 Thiago Dantas. All rights reserved.
+//
+
+#include "Game.h"
+
+int main(int argc, const char * argv[]) {
+    std::cout << "main()\n";
+    Game::GetInstance().Run();
+
+    return 0;
+}
