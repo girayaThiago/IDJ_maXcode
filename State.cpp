2f8d0351a0016d01ea865b000aa6ac0b26a27724
Thiago Dantas <giraya.thiago@gmail.com>
2018-03-15 14:27:56 -0300

    criada classe state

---
+++ State.cpp
@@ -0,0 +1,41 @@
+//
+//  State.cpp
+//  IDJ_2018
+//
+//  Created by Thiago Dantas on 11/03/18.
+//  Copyright © 2018 Thiago Dantas. All rights reserved.
+//
+
+#include <iostream>
+
+#include "State.h"
+
+State::State() {
+    quitRequested = false;
+//    std::cout << "State initialized" << std::endl;
+    bg = Sprite();
+    music = Music();
+
+}
+
+void State::LoadAssets() {
+    printf("LoadAssets()\n");
+    music.Open("./audio/stageState.ogg");
+    bg.Open("./img/ocean.jpg");
+    
+
+}
+
+void State::Update(float dt){
+    if (SDL_QuitRequested()){
+        quitRequested = true;
+    }
+}
+
+void State::Render(){
+    bg.Render(0,-0);
+}
+
+bool State::QuitRequested() {
+    return quitRequested;
+}
