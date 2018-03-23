81772eefaee880951fcab2c79934f037599e97d4
Thiago Dantas <giraya.thiago@gmail.com>
2018-03-23 15:01:19 -0300

    arquivos da classe sound criados, TODO implementar

---
+++ include/Sound.h
@@ -0,0 +1,21 @@
+//
+//  Sound.h
+//  linkSDL
+//
+//  Created by Thiago Dantas on 21/03/18.
+//  Copyright © 2018 Thiago Dantas. All rights reserved.
+//
+#pragma once
+
+#ifndef Sound_h
+#define Sound_h
+
+#include <stdio.h>
+#include "../include/Component.h"
+
+class Sound{
+public:
+    
+};
+
+#endif /* Sound_h */
---
+++ src/Sound.cpp
@@ -0,0 +1,9 @@
+//
+//  Sound.cpp
+//  linkSDL
+//
+//  Created by Thiago Dantas on 21/03/18.
+//  Copyright © 2018 Thiago Dantas. All rights reserved.
+//
+
+#include "../include/Sound.h"
